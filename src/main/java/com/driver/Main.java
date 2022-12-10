07782b8a2c3bfcd89bb04040c293215e9330fa6f
nikhil vanamala <nickhil215@gmail.com>
2022-12-10 22:02:27 +0530

    massage

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public String hh(){
+            return "Invoking method from class A";
+        }
+    }
+public static class B extends A{
+        public String hh(){
+            return "Method is overridden in Extendend class B";
+        }
+}
+public static void main(String [] args){
+        B b=new B();
+        b.hh();
+}
   
 }
